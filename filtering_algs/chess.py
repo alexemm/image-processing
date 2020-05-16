98f856b72caf6ccb82f0dd1860d3c80e65d3c4a2
Alexander Emmerich <al_emmerich@gmx.de>
2020-05-16 08:45:37 +0200

    Refactor chess and create chess test

--- filtering_algs/chess.py
+++ filtering_algs/chess.py
@@ -1,5 +1,4 @@
 import numpy as np
-import cv2
 
 
 def dimm(x, brightness=0.6):
@@ -10,52 +9,39 @@ def lighten(x, brightness=1.4):
     return x * brightness if x * brightness < 255 else 255
 
 
-# Load an color image in grayscale
-img = cv2.imread('lena512color.tiff', cv2.IMREAD_COLOR)
-rbg_img = cv2.cvtColor(img, cv2.IMREAD_COLOR)
-
-print('loaded img')
-
-max_size = 512
-grid_size = int(max_size/8)
-
-
-def f(grid_size=grid_size):
-    #for i in range()
+def f(grid_size: int):
     for i in range(grid_size):
-        if ((i % 2) == 0 and (int(i / 8) % 2 == 0) or ((i % 2 != 0) and int(i / 8) % 2 != 0)):
+        if (i % 2) == 0 and (int(i / 8) % 2 == 0) or ((i % 2 != 0) and int(i / 8) % 2 != 0):
             yield i
 
-alpha = np.array([0.25, 0.25, 0.25]) * 4
-channels = 3
-grids = [(x, y) for x in range(0, max_size , int(grid_size)) for y in range(0, max_size , int(grid_size))]
-#grids = [rbg_img[x: x + int(grid_size)][y: y + int(grid_size)] for x in range(0, 512, int(grid_size)) for y in range(0, 512, int(grid_size))]
 
-dark_grid_edges = [grids[idx]for idx in f()]
-dark_grids = [(range(dark_grid[0], dark_grid[0] + grid_size), range(dark_grid[1], dark_grid[1] + grid_size)) for dark_grid in dark_grid_edges]
+def squarize(img: np.ndarray) -> np.ndarray:
+    sqrWidth = np.ceil(np.sqrt(img.shape[0]*img.shape[1])).astype(int)
+    im_resize = img.reshape((sqrWidth, sqrWidth, 3))
+    return im_resize
 
 
-#img_grids = [dimm(img[x, y]) if (x+y) % 2 == 0 else lighten(img[x, y]) for y in range(img.shape[0]) for x in range(img.shape[1])]
-new_img = np.zeros(img.shape, dtype='uint8')
-level = 0.8
-lighten_grade = 1 + level
-darken_grade = 1 - level
-#import pdb; pdb.set_trace();
-for x in range(img.shape[1]):
-    for y in range(img.shape[0]):
-        darken = False
+def chess(img: np.ndarray, level: float = 0.8) -> np.array:
+    square_img = squarize(img)
+    #alpha = np.array([0.25, 0.25, 0.25]) * 4
+    #channels = square_img.shape[2]
+    max_size = square_img.shape[1]
+    grid_size = int(max_size / 8)
+    grids = [(x, y) for x in range(0, max_size, int(grid_size)) for y in range(0, max_size, int(grid_size))]
+    dark_grid_edges = [grids[idx] for idx in f(grid_size)]
+    dark_grids = [(range(dark_grid[0], dark_grid[0] + grid_size), range(dark_grid[1], dark_grid[1] + grid_size)) for
+                  dark_grid in dark_grid_edges]
+    new_img = np.zeros(img.shape, dtype='uint8')
 
-        for dark_grid in dark_grids:
-
-            if x in dark_grid[0] and y in dark_grid[1]:#(x+y) % 2 == 0:
-
-                new_img[x, y] = dimm(img[x, y, 2], brightness=darken_grade)#[:, :, 2])#(alpha.dot(img[x, y])/channels)
-                darken = True
-        if not darken:
-            new_img[x, y] = lighten(img[x, y, 2], brightness=lighten_grade)#[:, :, 2])#(alpha.dot(img[x, y])/channels)
-print('created new img...\nNow show')
-#h, w = new_img.shape
-#newer_img = cv2.cvtColor(new_img, cv2.COLOR_GRAY2BGR)
-cv2.imshow('image', new_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    lighten_grade = 1 + level
+    darken_grade = 1 - level
+    for x in range(img.shape[1]):
+        for y in range(img.shape[0]):
+            darken = False
+            for dark_grid in dark_grids:
+                if x in dark_grid[0] and y in dark_grid[1]:  # (x+y) % 2 == 0:
+                    new_img[x, y] = dimm(img[x, y, 2], brightness=darken_grade)
+                    darken = True
+            if not darken:
+                new_img[x, y] = lighten(img[x, y, 2], brightness=lighten_grade)
+    return new_img
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from filtering_algs.chess import chess
 from filtering_algs.k_means_filtering import cluster_img
 from filtering_algs.contrast_partition import partition
 from utils.img_tools import load_img, plot_img, save_img
@@ -23,5 +24,16 @@ def test_partition() -> None:
     save_img('new_imgs/test2.png', new_img)
 
 
+def test_chess() -> None:
+    dir = 'imgs/'
+    img = 'Lenna_(test_image).png'
+    #img = 'chang.jpeg'
+    img = load_img(dir + img, transparent=False)
+    new_img = chess(img, 0.5)
+    #plot_img(new_img)
+    save_img('new_imgs/test3.png', new_img)
+
+
 if __name__ == '__main__':
-    test_partition()
+    #test_partition()
+    test_chess()
